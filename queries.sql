abf63a914acbdc56ae6163ba6261b17dc053be74
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-03-26 08:37:50 -0500

    Completed task three with passing TestTaskThree

--- queries.sql
+++ queries.sql
@@ -1,12 +1,18 @@
 -- Part 1: Test it with SQL
---    int id
---    String employer
---    String name
---    String skills
 
+--    id int,
+--    employer varchar(255),
+--    name varchar(255),
+--    skills varchar(255)
 
 -- Part 2: Test it with SQL
 
+    SELECT name
+    FROM employer
+    WHERE location = "St. Louis City";
+
 -- Part 3: Test it with SQL
+
+    DROP TABLE job;
 
 -- Part 4: Test it with SQL
