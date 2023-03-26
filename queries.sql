fa4ec7c993af7ef80737a0750e50864fd01f953d
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-03-26 12:55:44 -0500

    Completed section Part 4 with passing TestTaskFour tests. Assignment completed with all passing tests and ready for submission.

--- queries.sql
+++ queries.sql
@@ -16,3 +16,9 @@
     DROP TABLE job;
 
 -- Part 4: Test it with SQL
+
+    SELECT *
+    FROM skill
+    INNER JOIN job_skills ON skill.id = job_skills.skills_id
+    ORDER BY name ASC;
+
--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,8 +2,10 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,6 +29,9 @@ public class HomeController {
     @Autowired
     private JobRepository jobRepository;
 
+    @Autowired
+    private SkillRepository skillRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -39,6 +44,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
@@ -57,10 +63,14 @@ public class HomeController {
         if (optEmployer.isPresent()){
             Employer employer = (Employer) optEmployer.get();
             newJob.setEmployer(employer);
-            jobRepository.save(newJob);
         }
 
+        if(skills != null){
+            List<Skill> selectedSkills = (List<Skill>) skillRepository.findAllById(skills);
+            newJob.setSkills(selectedSkills);
+        }
 
+        jobRepository.save(newJob);
         return "redirect:";
     }
 
